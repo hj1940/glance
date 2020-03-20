c93495d7b76aca2c491bb9dda8623e90d067393f
hj1940 <ttlle12@naver.com>
2020-03-20 13:50:42 +0900

    Updates

---
+++ precache-manifest.533ad68765316821d9ab01379e8cb7d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf39571e114d1a4bb5a964fae96f5fa8",
+    "url": "/glance/index.html"
+  },
+  {
+    "revision": "12ce15a7b715d617f0bf",
+    "url": "/glance/static/css/main.43f2d972.chunk.css"
+  },
+  {
+    "revision": "75b735a1955470ba3535",
+    "url": "/glance/static/js/2.215b0865.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/glance/static/js/2.215b0865.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12ce15a7b715d617f0bf",
+    "url": "/glance/static/js/main.b456f5b3.chunk.js"
+  },
+  {
+    "revision": "686d0b65768db57e99b6",
+    "url": "/glance/static/js/runtime-main.5fa03152.js"
+  }
+]);
